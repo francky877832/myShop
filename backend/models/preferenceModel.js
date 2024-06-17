a69328ad38a5ca89cd1b50653c94577aa04d9f03
francky <francky877832@gmail.com>
2024-06-17 14:56:38 +0300

    Mise a jour du model, routes, controllers de preference  Changes to be committed: 	modified:   backend/controllers/preferenceController.js 	modified:   backend/models/preferenceModel.js     modified:   backend/routes/appRoutes.js

--- backend/models/preferenceModel.js
+++ backend/models/preferenceModel.js
@@ -6,10 +6,15 @@ const Schema = mongoose.Schema
 const preferenceSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     preferences : [{
-                category : { type : Schema.Types.ObjectId, ref : 'Category', required : true  },
+                //category : { type : Schema.Types.ObjectId, ref : 'Category', required : true  },
+                /*//POUR LES TESTS*/
+                category : { type : String, required : true  },
+                
                 createdAt : { type : Date, default : Date.now },
                 updatedAt : { type : Date, default : Date.now },
             }],
+    createdAt : { type : Date, default : Date.now },
+    updatedAt : { type : Date, default : Date.now },
 });
 
 module.exports = mongoose.model('Preference', preferenceSchema);
--- backend/routes/appRoutes.js
+++ backend/routes/appRoutes.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 
 const prodCtrl = require('../controllers/productController');
+const prefCtrl = require('../controllers/preferenceController');
 
-router.get('/products/', prodCtrl.getAllProduct);
+router.get('/products', prodCtrl.getAllProduct);
 router.get('/product/:id', prodCtrl.getOneProduct);
-router.post('/product/', prodCtrl.createProduct);
+router.post('/product', prodCtrl.createProduct);
 router.put('/product/:id', prodCtrl.modifyProduct);
 router.delete('/product/:id', prodCtrl.deleteProduct);
 /*
@@ -17,4 +18,11 @@ router.get('/:id', auth, prodCtrl.getOneThing);
 router.put('/:id', auth, prodCtrl.modifyThing);
 router.delete('/:id', auth, prodCtrl.deleteThing);
 */
+
+//Preferences route
+router.get('/preferences/get/:id', prefCtrl.getUserPreferences);
+router.post('/preferences/add/:id', prefCtrl.addUserPreferences);
+router.put('/preferences/modify/:id', prefCtrl.modifyUserPreferences);
+
+
 module.exports = router;
