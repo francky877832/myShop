fe653ae500b6cb29bc65cc442e4ecb8a65163acb
francky <francky877832@gmail.com>
2024-07-03 09:12:52 +0300

    Mise a jour des dependance et react DevTools

--- frontend/jiraShopping/App.js
+++ frontend/jiraShopping/App.js
@@ -1,11 +1,14 @@
+import { StrictMode } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import MainNavigation from './src/navigation/MainNavigation';
 
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex:1,top:StatusBar.currentHeight,}}>
-      <MainNavigation/>
-    </SafeAreaView>
+    <StrictMode>
+      <SafeAreaView style={{flex:1,top:StatusBar.currentHeight,}}>
+        <MainNavigation/>
+      </SafeAreaView>
+    </StrictMode>
   );
 }
--- frontend/js test/jiraShopping/index.js
+++ frontend/js test/jiraShopping/index.js
@@ -1,9 +1,24 @@
 /**
  * @format
  */
-
+/*
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+*/
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { StrictMode } from 'react';
+import App from './App';
+import { name as appName } from './app.json';
+
+const StrictApp = () => (
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+AppRegistry.registerComponent(appName, () => StrictApp);
