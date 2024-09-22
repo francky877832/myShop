aa283fc034aa7d399ad0aeddff2a670ced33e4ca
francky <francky877832@gmail.com>
2024-09-22 22:35:16 +0300

    Changes to be committed: 	modified:   frontend/expo-update.txt 	modified:   frontend/jiraShopping/src/remote/server.js

--- frontend/jiraShopping/src/remote/server.js
+++ frontend/jiraShopping/src/remote/server.js
@@ -1,9 +1,9 @@
 export const appName = 'jirashopping'
 //export const server = 'http://192.168.165.254:3000'
-export const server = 'http://192.168.73.254:3000'
+//export const server = 'http://192.168.73.254:3000'
 export const appStoreUrl = 'https://play.google.com/store/apps/details?id=com.jira877832.jiraShopping';
 
-//export const server = 'https://jirashopping-25ee1eaa49ff.herokuapp.com'
+export const server = 'https://jirashopping-25ee1eaa49ff.herokuapp.com'
 
 export const noImagePath = `${server}/userApp/assets/images/products/empty-img.png`
 
